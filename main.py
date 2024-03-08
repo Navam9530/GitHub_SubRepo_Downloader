34f6452521fc7dccb49dd776de0f99df25a4eabc
Nava Teja <mushamnavam9530@gmail.com>
2024-03-08 14:33:10 +0530

    Modified according to PEP8

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-# Downloading the Required Packages
-import os, platform
-os.system('pip install customtkinter gitdir PyGithub')
+"""This module runs a tkinter GUI for downloading GitHub Sub-Repositories and files inside them"""
 
-# Importing the Necessary Libraries
+import os
+from tkinter import messagebox as mb
 import customtkinter as ctk
 from github import Github
-from tkinter import messagebox as mb
-if platform.system() == 'Windows':
-    os.system('cls')
-else:
-    os.system('clear')
+from github.GithubException import UnknownObjectException
 
 # Window Specifications
 root = ctk.CTk()
@@ -18,13 +13,17 @@ root.geometry('600x600')
 root.resizable(False, False)
 
 # Download Logic
-def downloadIt():
+def download_it():
+    """Downloads the contents from the limk provided"""
+
     try:
         repo_link = link.get().split('/')
         repo_link = f'{repo_link[3]}/{repo_link[4]}'
         Github().get_repo(repo_link)
-    except:
-        mb.showerror('ERROR', "The Link pasted doesn't consist any GitHub Repositories. Make sure the link is valid and try again!")
+    except UnknownObjectException:
+        mb.showerror('ERROR', "The Link pasted doesn't consist any GitHub Repositories!")
+    except IndexError:
+        mb.showerror('ERROR', "The Link pasted doesn't consist any GitHub Repositories!")
     else:
         os.system(f'gitdir {link.get()}')
         mb.showinfo('SUCCESS', 'Download Succeeded!')
@@ -34,7 +33,8 @@ ctk.CTkLabel(root, text='Paste the Link below').place(relx=0.5, rely=0.4, anchor
 link = ctk.StringVar()
 textbox = ctk.CTkEntry(root, width=360, height=40, textvariable=link)
 textbox.place(relx=0.2, rely=0.5)
-ctk.CTkButton(root, height=30, text='Download', command=downloadIt).place(relx=0.5, rely=0.7, anchor='center')
+button = ctk.CTkButton(root, height=30, text='Download', command=download_it)
+button.place(relx=0.5, rely=0.7, anchor='center')
 
 # For Fast Pasting
 root.update()
